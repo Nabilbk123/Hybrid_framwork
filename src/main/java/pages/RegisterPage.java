b112e79cea24c10dd406bed630b3ccfac7d03f04
Nabil_BK~ <6212574@bdeb.qc.ca~>
2023-08-28 13:17:46 -0400

    Automatisation Selenium

---
+++ src/main/java/pages/RegisterPage.java
@@ -0,0 +1,17 @@
+// Le concept de Page Object Model
+// Cette classe va contenir tout nos elements a tester dans cette page
+// On va par exemple creer une page pour : Login (a partir de 'my account')
+// Une autre pour : Register (a partir de 'my account')
+
+
+
+
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegisterPage {
+	//Le consol de Page Object Model
+	WebDriver driver;
+
+}
